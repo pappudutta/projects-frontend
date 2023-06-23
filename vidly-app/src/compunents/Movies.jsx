50c95bf9c8b077bca7010556630cc0ff1c3517dd
Pappu Dutta <pappudutta279@gmail.com>
2023-06-23 20:56:13 +0530

    feat: movie list add with delete feature

---
+++ vidly-app/src/compunents/Movies.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+  // Testing Purpose
+  //   handleDelete = movie => {
+  //     const movies = this.state.movies;
+  //     const index = movies.findIndex(i => i._id === movie._id);
+  //     if (index !== -1) {
+  //       movies.splice(index, 1);
+  //       this.setState(movies);
+  //     }
+  //   };
+  // Testing Purpose
+
+  render() {
+    const { length: count } = this.state.movies;
+
+    return (
+      <div>
+        <h3>
+          {count === 0
+            ? "There is no movie in the list."
+            : `There are ${count} movies in this list.`}
+        </h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td> {movie.title} </td>
+                <td> {movie.genre.name} </td>
+                <td> {movie.numberInStock} </td>
+                <td> {movie.dailyRentalRate} </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
