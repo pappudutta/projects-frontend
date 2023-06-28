90dd9388b64e48c5971a0ac47447767535f6284b
Pappu Dutta <pappudutta279@gmail.com>
2023-06-28 11:50:08 +0530

    refactor: Like compunent changed to state less functional compunent

--- vidly-app/src/compunents/common/Like.jsx
+++ vidly-app/src/compunents/common/Like.jsx
@@ -1,21 +1,14 @@
 import React, { Component } from "react";
 
-class Like extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <i
-          style={{ cursor: "pointer" }}
-          onClick={this.props.onClick}
-          className={
-            !this.props.liked ? "fa-heart fa-regular" : "fa-heart fa-solid"
-          }
-        ></i>
-        {/* <i className="fa-heart fa-regular"></i> */}
-      </div>
-    );
-  }
+export default function like(props) {
+  return (
+    <div>
+      <i
+        style={{ cursor: "pointer" }}
+        onClick={props.onClick}
+        className={!props.liked ? "fa-heart fa-regular" : "fa-heart fa-solid"}
+      ></i>
+      {/* <i className="fa-heart fa-regular"></i> */}
+    </div>
+  );
 }
-
-export default Like;
